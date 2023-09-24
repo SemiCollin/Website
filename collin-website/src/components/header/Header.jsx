27dec9f76f9b1994ea1aa0041e511c48f301c025
SemiCollin <ckwan26.email@gmail.com>
2023-09-24 01:36:48 -0400

    defined css styles, began initial build setup for header component

--- collin-website/src/components/header/Header.jsx
+++ collin-website/src/components/header/Header.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import Socials from "./Socials";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Collin Kwan</h1>
+        <h5 className="text-light">Mechatronics Engineer</h5>
+        <CTA />
+        <Socials />
+
+        <div className="me">
+          <img src="" alt="" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
